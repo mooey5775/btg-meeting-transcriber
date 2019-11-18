f6289ef46e744d3529f9b1fde7f73ec6073c94be
Edward Li <edwardyaoli@gmail.com>
2019-11-18 17:13:04 -0500

    added google demo

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,1 +1,1 @@
-export GOOGLE_APPLICATION_CREDENTIALS='/Users/edwardli/Documents/Code/BTG/meeting_transcriber/btg-test-4588da31d3e5.json'
+export GOOGLE_APPLICATION_CREDENTIALS='/Users/edwardli/Documents/Code/BTG/meeting_transcriber/tests/btg-test-4588da31d3e5.json'
